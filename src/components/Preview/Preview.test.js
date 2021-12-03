d73d2fa09932358cfcc2548dd4fd153fa57f2020
Chase Crowder <gcrowder93@gmail.com>
2021-12-02 16:50:28 -0800

    app test

--- src/components/Preview/Preview.test.js
+++ src/components/Preview/Preview.test.js
@@ -2,6 +2,6 @@ import { render } from '@testing-library/react';
 import Preview from './Preview';
 
 test(`renders the Preview panel`, () => {
-  const container = render(<Preview head="duck" middle="pink" bottom="blue" />);
+  const container = render(<Preview head="duck" middle="red" bottom="white" />);
   expect(container).toMatchSnapshot();
 });
