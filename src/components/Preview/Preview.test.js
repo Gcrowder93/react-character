bbbfd9228fb077cd32a523824e0efb2e1eb8e9db
Chase Crowder <gcrowder93@gmail.com>
2021-12-02 17:11:00 -0800

    fixed a test

--- src/components/Preview/Preview.test.js
+++ src/components/Preview/Preview.test.js
@@ -2,6 +2,6 @@ import { render } from '@testing-library/react';
 import Preview from './Preview';
 
 test(`renders the Preview panel`, () => {
-  const container = render(<Preview head="duck" middle="red" bottom="white" />);
+  const container = render(<Preview head="duck" middle="blue" bottom="blue" />);
   expect(container).toMatchSnapshot();
 });
